06a36021364fee2600aa637a30dba66d27403d8c
wataru-kurashima <wataru-kurashima@hands-lab.com>
2019-05-05 21:45:22 +0900

    bash support

--- workspace/9cc/test.bash
+++ workspace/9cc/test.bash
@@ -1,4 +1,4 @@
-##!/bin/bash
+#!/bin/bash
 try() {
   expected="$1"
   input="$2"
@@ -8,10 +8,10 @@ try() {
   ./tmp
   actual="$?"
 
-  if [ "$actual" = "$expected" ]; then
-    echo "$input => $actual"
+  if [[ "${actual}" = "${expected}" ]]; then
+    echo "${input} => ${actual}"
   else
-    echo "$expected expected, but got $actual"
+    echo "${expected} expected, but got ${actual}"
     exit 1
   fi
 }
